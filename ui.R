573abeba9da9a72bd04f7b83bc1358ef1f8abab3
DDSQ <deciox@hotmail.com>
2018-06-05 13:11:12 +0200

    ui.R aangepast - tabitems added to body - tabmenu added to sidebar

--- ui.R
+++ ui.R
@@ -17,10 +17,44 @@ dashboardPage(
   
   dashboardSidebar(
     sidebarMenu(
+      menuItem("The origin of the Data",
+               tabName = "origin",
+               icon = icon("google")),
+      menuItem("Research Questions",
+               tabName = "question",
+               icon = icon("google")),
+      menuItem("R packages",
+               tabName = "package",
+               icon = icon("google")),
+      menuItem("Data Overview",
+               tabName = "data_overviw",
+               icon = icon("google")),
+      menuItem("Visualizations",
+               tabName = "visualizations",
+               icon = icon("google"))
       
     )
   ),
   
-dashboardBody(),
+dashboardBody(
+  tabItems(
+    tabItem(tabName = "origin",
+            h2("The origin of the data")
+            ),
+    tabItem(tabName = "question",
+            h2("The research questions")
+            ),
+    tabItem(tabName = "package",
+            h2("Elaboration on used packages")
+            ),
+    tabItem(tabName = "data_overview",
+            h2("Overview of the data")
+            ),
+    tabItem(tabName = "visualizations",
+            h2("Data visualizations")
+            )
+  )
+  
+),
 )
 
