d886dfc044c9bfbe2fc2f996c51d5baa83744f22
Iceteagreen <38423730+Iceteagreen@users.noreply.github.com>
2018-06-05 12:05:42 +0200

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,26 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(
+    title = "Cryptocurreny Dashboard",
+    titleWidth = 350
+  ),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      
+    )
+  ),
+  
+dashboardBody(),
+)
+
